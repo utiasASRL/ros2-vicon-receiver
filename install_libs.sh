60664e570e875fb274d7273ec876159b442b0a27
Frederike Duembgen <frederike.dumbgen@robotics.utias.utoronto.ca>
2023-03-28 21:05:54 -0400

    Attempt to fix install

--- install_libs.sh
+++ install_libs.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
-echo "Installing shared libraries, please wait"
-sudo cp DataStreamSDK_10.1/libViconDataStreamSDK_CPP.so /usr/lib
-echo "."
-sudo cp DataStreamSDK_10.1/libboost_system-mt.so.1.58.0 /usr/lib
-echo "."
-sudo cp DataStreamSDK_10.1/libboost_thread-mt.so.1.58.0 /usr/lib
-echo "."
-sudo cp DataStreamSDK_10.1/libboost_timer-mt.so.1.58.0 /usr/lib
-echo "."
-sudo cp DataStreamSDK_10.1/libboost_chrono-mt.so.1.58.0 /usr/lib
-echo "."
-sudo chmod 0755 /usr/lib/libViconDataStreamSDK_CPP.so /usr/lib/libboost_system-mt.so.1.58.0 /usr/lib/libboost_thread-mt.so.1.58.0 /usr/lib/libboost_timer-mt.so.1.58.0 /usr/lib/libboost_chrono-mt.so.1.58.0
-echo "." 
+#!/bin/bash
+
+DIR="/usr/lib/"
+DIR="$HOME/mambaforge/envs/ros_humble/lib/"
+
+echo $DIR
+
+echo "Installing shared libraries to $DIR, please wait"
+for f in libViconDataStreamSDK_CPP.so libboost_system-mt.so.1.58.0 libboost_thread-mt.so.1.58.0 libboost_timer-mt.so.1.58.0 libboost_chrono-mt.so.1.58.0 
+do
+  sudo cp "DataStreamSDK_10.1/$f" "$DIR"
+  sudo chmod 0755 "$DIR/$f"
+  echo "."
+done
+
 sudo ldconfig
 echo "."
 echo "Installlation finished"
-
